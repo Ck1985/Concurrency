af8835d52b34d12eb3cddbc1425679bd1753104a
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-08-18 15:11:39 +0700

    New Commit

--- src/example/Example_9.java
+++ src/example/Example_9.java
@@ -12,12 +12,25 @@ class PriorityFromFactory implements Runnable {
         return Thread.currentThread() + ": " + this.countDown;
     }
     public void run() {
-        for (int i = 0; i < 100000; i++) {
+        while (true) {
+            for (int i = 0; i < 100000; i++) {
+                d += (Math.PI + Math.E) % (double)i;
+                if (i % 1000 == 0) {
+                    Thread.yield();
+                }
+            }
+            System.out.println(this);
+            if (--countDown == 0) return;
         }
     }
 }
 public class Example_9 {
     public static void main(String[] args) {
-
+        ExecutorService exec = Executors.newCachedThreadPool(new PriorityThreadFactory());
+        for (int i = 0; i < 5; i++) {
+            exec.execute(new PriorityFromFactory());
+        }
+        exec.execute(new PriorityFromFactory());
+        exec.shutdown();
     }
 }
