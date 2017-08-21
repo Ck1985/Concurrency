d0f655764407dc91ba9ea1bb3ef816809f4b5815
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-08-21 20:54:59 +0700

    New Commit

---
+++ src/concurrency/ResponsiveUI.java
@@ -0,0 +1,13 @@
+package concurrency;
+
+/**
+ * Created by anonymous.vn1985@gmail.com
+ */
+class UnResponsiveUI {
+    private volatile double d = 1;
+}
+public class ResponsiveUI {
+    public static void main(String[] args) {
+
+    }
+}
