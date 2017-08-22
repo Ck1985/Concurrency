78b298f174a3102f259acec070f93d59354268b7
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-08-22 22:14:39 +0700

    New Commit

--- src/concurrency/ResponsiveUI.java
+++ src/concurrency/ResponsiveUI.java
@@ -5,8 +5,24 @@ package concurrency;
  */
 class UnResponsiveUI {
     private volatile double d = 1;
+    public UnResponsiveUI() throws Exception {
+        while (d > 0) {
+            d = d + (Math.PI + Math.E) / d;
+        }
+        System.in.read();
+    }
 }
-public class ResponsiveUI {
+public class ResponsiveUI extends Thread {
+    private static volatile double d = 1;
+    public ResponsiveUI() {
+        this.setDaemon(true);
+        this.start();
+    }
+    public void run() {
+        while (true) {
+            d = d + (Math.PI + Math.E) / d;
+        }
+    }
     public static void main(String[] args) {
 
     }
